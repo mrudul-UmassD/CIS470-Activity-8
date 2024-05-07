f83f6a9bba293f9e8d779d5531dbab90ab0e25ec
mrudul-UmassD <158495104+mrudul-UmassD@users.noreply.github.com>
2024-05-07 21:58:11 +0000

    refactor: optimize route handling in index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const { classifyTriangle } = require('./classifyTriangle');
 
 const app = express();
 
-const { classifyTriangle } = require('./classifyTriangle');
 
 app.use(express.json());
 
@@ -11,13 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('https://y8bmt2ykj4.execute-api.us-east-2.amazonaws.com/default/CIS470-Activity-8', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-app.post('https://y8bmt2ykj4.execute-api.us-east-2.amazonaws.com/default/CIS470-Activity-8', (req, res) => {
+app.post('/', (req, res) => {
     const { side1, side2, side3 } = req.body;
-    const result = classifyTriangle(side1,side2,side3);
+    const result = classifyTriangle(side1, side2, side3);
     res.json({ result });
 })
 
