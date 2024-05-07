0244918fb851d8750afb2c2706ccc8429a1658a0
mrudul-UmassD <158495104+mrudul-UmassD@users.noreply.github.com>
2024-05-07 19:08:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const { classifyTriangle } = require('./classifyTriangle');
 
 const app = express();
 
+const { classifyTriangle } = require('./classifyTriangle');
 
 app.use(express.json());
 
@@ -17,7 +17,7 @@ app.get('/default/CIS470-Activity-8', (req, res) => {
 
 app.post('/default/CIS470-Activity-8/check-triangle', (req, res) => {
     const { side1, side2, side3 } = req.body;
-    const result = classifyTriangle(side1, side2, side3);
+    const result = classifyTriangle(side1,side2,side3);
     res.json({ result });
 })
 
