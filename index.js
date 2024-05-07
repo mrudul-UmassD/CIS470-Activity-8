63af364b5dee4fff322d447984ae5482baca5da5
mrudul-UmassD <158495104+mrudul-UmassD@users.noreply.github.com>
2024-05-07 19:05:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('https://y8bmt2ykj4.execute-api.us-east-2.amazonaws.com/default/CIS470-Activity-8', (req, res) => {
+app.get('/default/CIS470-Activity-8', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-app.post('https://y8bmt2ykj4.execute-api.us-east-2.amazonaws.com/default/CIS470-Activity-8', (req, res) => {
+app.post('/default/CIS470-Activity-8/check-triangle', (req, res) => {
     const { side1, side2, side3 } = req.body;
     const result = classifyTriangle(side1, side2, side3);
     res.json({ result });
